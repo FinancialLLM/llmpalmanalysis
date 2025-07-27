c22bb5264783bb7d8c8f1e3418575423f4924f8d
FinancialLLM <financialllm19@gmail.com>
2025-07-27 15:06:30 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,40 +2,100 @@ import streamlit as st
 import base64
 from openai import OpenAI
 
+import smtplib
+from email.message import EmailMessage
+
+from markdown_pdf import MarkdownPdf
+from markdown_pdf import Section
+
+def save_email(receiver):
+    file_path = "emails.txt"
+
+    with open(file_path, "r") as file:
+        existing_emails = [email.strip() for email in file.readlines()]
+
+    if receiver not in existing_emails: 
+        with open(file_path, 'a') as file:
+            file.write(f"{receiver}\n")
+
+def send_email(receiver, response):
+    try:
+        save_email(receiver)
+
+        pdf = MarkdownPdf(toc_level = 2, optimize = True)
+        pdf.add_section(Section(response))
+        pdf.save(f"{receiver}.pdf")
+
+        msg = EmailMessage()
+        msg["Subject"] = "PalmAanalysis.ai Report"
+        msg["From"] = st.secrets.email_sender
+        msg["To"] = receiver
+        msg.set_content("Dear Sir/Madam,\n\nHere is the report from PalmAnalysis.ai.\n\nThank you.\n\nSincere regards,\n\nPalmAnalysis.ai")
+        
+        file_data = open(f"{receiver}.pdf", "rb").read()
+        msg.add_attachment(file_data, maintype = "application", subtype = "octet-stream", filename = "palmAnalysis.pdf")
+
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+            smtp.login(st.secrets.email_sender, st.secrets.email_password)
+            smtp.send_message(msg)
+
+        return True
+
+    except Exception as e:
+        return e
+
 st.image("logo.png")
 st.title("PalmAnalysis.ai : Discovering Your Strength, Empower Your Future")
 
 enable = st.checkbox("Enable camera")
 img = st.camera_input("Take a picture", disabled = not enable)
 
-# Function to encode the image
-def encode_image(image):
-    return base64.b64encode(image).decode("utf-8")
-
-api_key = st.sidebar.text_input("OpenAI API Key", type = "password")
+api_key = st.secrets.openai_api_key
+if "has_response" not in st.session_state:
+    st.session_state.has_response = False
 
 if img:
+    with st.form("my_form"):
+        if not st.session_state.has_response:
+            client = OpenAI(api_key = api_key)
 
-    client = OpenAI(api_key = api_key)
+            img = img.getvalue()
+            base64_image = base64.b64encode(img).decode("utf-8")
 
-    img = img.getvalue()
-    base64_image = encode_image(img)
-
-    response = client.responses.create(
-        model="gpt-4.1",
-        input=[
-            {
-                "role": "user",
-                "content": [
-                    { "type": "input_text", "text": "You are now a fortune teller. Please help me read this person’s palm and tell me about his traits and personality. Then, give me advice on what kind of career suits him, as well as insights into his destiny, health, wealth, love life, and family. This person is male." },
+            response = client.responses.create(
+                model="gpt-4.1",
+                input=[
                     {
-                        "type": "input_image",
-                        "image_url": f"data:image/jpeg;base64,{base64_image}",
-                    },
+                        "role": "user",
+                        "content": [
+                            { "type": "input_text", "text": "You are now a fortune teller. Please help me read this person’s palm and tell me about his traits and personality. Then, give me advice on what kind of career suits him, as well as insights into his destiny, health, wealth, love life, and family. This person is male." },
+                            {
+                                "type": "input_image",
+                                "image_url": f"data:image/jpeg;base64,{base64_image}",
+                            },
+                        ],
+                    }
                 ],
-            }
-        ],
-    )
+            )
 
-    st.header("Response")
-    st.write(response.output_text)
+            st.session_state.palm_res = response.output_text
+
+        st.session_state.has_response = True
+        
+        st.header("Response")
+        st.write(st.session_state.palm_res)
+
+        st.header("Send Report")
+        
+        receiver = st.text_input("Email")
+        submitted = st.form_submit_button("Send")
+
+        if submitted: 
+            msg = send_email(receiver, st.session_state.palm_res)
+            if msg == True:
+                st.info("The report is sent. ")
+            else:
+                st.error(msg)
+
+else:
+    st.session_state.has_response = False
